af3468373bb7610945495cb571bfd296326f0468
Londar28 <lukinivan17@mail.ru>
2023-05-17 23:50:46 +0800

    initial commit

---
+++ renderer.js
@@ -0,0 +1,65 @@
+
+// document.getElementById("preview").onclick = function () {
+//     document.getElementById("mainwindow").setAttribute('src', 'preview.html')
+// }
+
+window.onload = () => {
+  console.log(window);
+  const buttonsFrame = document.getElementById("buttonsFrame")
+  const buttondoxy = buttonsFrame.contentDocument.documentElement.getElementsByTagName("button")[0]
+  const buttonsoat = buttonsFrame.contentDocument.documentElement.getElementsByTagName("button")[1]
+  const buttontgpr = buttonsFrame.contentDocument.documentElement.getElementsByTagName("button")[2]
+  const buttonplos = buttonsFrame.contentDocument.documentElement.getElementsByTagName("button")[3]
+  const buttondoo = buttonsFrame.contentDocument.documentElement.getElementsByTagName("button")[4]
+  const buttonymt = buttonsFrame.contentDocument.documentElement.getElementsByTagName("button")[5]
+  const buttonyp = buttonsFrame.contentDocument.documentElement.getElementsByTagName("button")[6]
+  const buttonyv = buttonsFrame.contentDocument.documentElement.getElementsByTagName("button")[7]
+  const buttondvt = buttonsFrame.contentDocument.documentElement.getElementsByTagName("button")[8]
+  const buttonyppap = buttonsFrame.contentDocument.documentElement.getElementsByTagName("button")[9]
+  const buttontest = buttonsFrame.contentDocument.documentElement.getElementsByTagName("button")[10]
+  // console.log(buttonFrame);
+  buttondoxy.addEventListener("click", () => {
+    // location.href="dlina_otrezka_x_y.html"
+    document.getElementById("mainwindow").setAttribute('src', 'dlina_otrezka_x_y.html')
+  })
+  buttonymt.addEventListener("click", () => {
+    // location.href="dlina_otrezka_x_y.html"
+    document.getElementById("mainwindow").setAttribute('src', 'yrav_median_triangle.html')
+  })
+  buttonyppap.addEventListener("click", () => {
+    // location.href="dlina_otrezka_x_y.html"
+    document.getElementById("mainwindow").setAttribute('src', 'yrav_pramo_per_and_par.html')
+  })
+  buttonyp.addEventListener("click", () => {
+    // location.href="dlina_otrezka_x_y.html"
+    document.getElementById("mainwindow").setAttribute('src', 'yrav_pramoy.html')
+  })
+  buttondvt.addEventListener("click", () => {
+    // location.href="dlina_otrezka_x_y.html"
+    document.getElementById("mainwindow").setAttribute('src', 'dlina_visot_triangle.html')
+  })
+  buttonyv.addEventListener("click", () => {
+    // location.href="dlina_otrezka_x_y.html"
+    document.getElementById("mainwindow").setAttribute('src', 'yrav_visot.html')
+  })
+  buttontest.addEventListener("click", () => {
+    // location.href="dlina_otrezka_x_y.html"
+    document.getElementById("mainwindow").setAttribute('src', 'preview.html')
+  })
+  buttontgpr.addEventListener("click", () => {
+    // location.href="dlina_otrezka_x_y.html"
+    document.getElementById("mainwindow").setAttribute('src', 'angel_pryam_proekciya.html')
+  })
+  buttonsoat.addEventListener("click", () => {
+    // location.href="dlina_otrezka_x_y.html"
+    document.getElementById("mainwindow").setAttribute('src', 'Sides_of_a_triangle.html')
+  })
+  buttonplos.addEventListener("click", () => {
+    // location.href="dlina_otrezka_x_y.html"
+    document.getElementById("mainwindow").setAttribute('src', 'ploshad_triangle.html')
+  })
+  buttondoo.addEventListener("click", () => {
+    // location.href="dlina_otrezka_x_y.html"
+    document.getElementById("mainwindow").setAttribute('src', 'del_otrezok_otnoshenie.html')
+  })
+}
